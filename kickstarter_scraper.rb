6a6f7b669c33c1e01d02e8e6fcda6a10eec334dd
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-12 16:26:02 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,6 +6,7 @@ def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   #projects: kickstarter.css("li.project.grid_4")
   #project.css("h2.bbcard_name strong a").text 
+  #project.css("div.project-thumbnail").attribute("src").value
   kickstarter = Nokogiri::HTML(html)
   
 binding.pry 
